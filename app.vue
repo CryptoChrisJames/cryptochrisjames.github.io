b4b4981319d975833bde71cb8511171262b60c9a
CryptoChrisJames <cryptochrisjames@CCJMBP.local>
2023-04-06 15:36:29 -0500

    Adding footer and header.

--- app.vue
+++ app.vue
@@ -1,14 +1,24 @@
 <template>
   <div class="container">
-    <div class="navbar">
-      <h1>CCJ BLOG</h1>
-      <ul>
-        <li><NuxtLink to="/about">About</NuxtLink></li>
-        <li><NuxtLink to="/contact">Contact</NuxtLink></li>
-      </ul>
+    <div class="header">
+      <div class="header-container">
+        <img src="./assets/images/header.jpg" alt="" class="header-img">
+        <div class="navbar">
+          <h1>CCJ BLOG</h1>
+          <ul>
+            <li><NuxtLink to="/about">About</NuxtLink></li>
+            <li><NuxtLink to="/contact">Contact</NuxtLink></li>
+          </ul>
+        </div>
+      </div>
     </div>
     <div class="content">
       <NuxtPage/>
+    </div>
+    <div class="header">
+      <div class="header-container">
+        <img src="./assets/images/footer.jpg" alt="" class="header-img">
+      </div>
     </div>
   </div>
 </template>
@@ -19,5 +29,12 @@
 body, html{
   background-color: $black;
   color: $white;
+  height: 100vh;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
 }
 </style>
