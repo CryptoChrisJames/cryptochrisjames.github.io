0c8cba1270d6cefe5a64a1efada4fd376533e689
CryptoChrisJames <22749214+CryptoChrisJames@users.noreply.github.com>
2023-04-06 09:23:13 -0500

    Trying renderer again. Adding nojekyll file.

--- pages/post/[category]/[post].vue
+++ pages/post/[category]/[post].vue
@@ -1,7 +1,11 @@
 <template>
-    <ContentDoc :path="`/${category}/${post}`" />
+    <ContentRenderer>
+        <ContentRendererMarkdown :value="data" />
+    </ContentRenderer>
 </template>
 
 <script setup>
 const { category, post } = useRoute().params;
+const { data } = await useAsyncData('post', 
+    () => queryContent(`/${category}/${post}`).findOne());
 </script>
