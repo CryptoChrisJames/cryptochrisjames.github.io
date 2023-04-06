4d11f428dfed52300b58a9774be701af1501497d
CryptoChrisJames <CryptoChrisJames@users.noreply.github.com>
2023-04-06 14:33:12 +0000

    deploy: 513c736a20713267585c15b52d0de62bb3f8a46f

--- _nuxt/Markdown.9023f052.js
+++
@@ -1,1 +0,0 @@
-import s from"./ContentSlot.70dc87f6.js";import{h as o,i as u,l as f,q as i}from"./entry.9fb5e524.js";import"./utils.3745fbb4.js";const l=o({name:"Markdown",extends:s,setup(t){const{parent:e}=i(),{between:n,default:a}=u(),r=f(()=>typeof t.unwrap=="string"?t.unwrap.split(" "):["*"]);return{fallbackSlot:a,tags:r,between:n,parent:e}}});export{l as default};
