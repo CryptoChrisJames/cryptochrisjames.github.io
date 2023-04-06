cb34a17634f10b9e97fbf3ba3cd36dc41e709fc3
CryptoChrisJames <22749214+CryptoChrisJames@users.noreply.github.com>
2023-04-06 17:15:24 -0500

    Creating nav

--- app.vue
+++ app.vue
@@ -1,27 +1,30 @@
 <template>
   <div class="container">
     <div class="header">
-      <div class="header-container">
-        <img src="./assets/images/header.jpg" alt="" class="header-img">
-        <div class="navbar">
-          <h1>CCJ BLOG</h1>
-          <ul>
-            <li><NuxtLink to="/about">About</NuxtLink></li>
-            <li><NuxtLink to="/contact">Contact</NuxtLink></li>
-          </ul>
+      <span @click="goHome()">
+        <div class="img-container">
+          <img src="./assets/images/header.jpg" alt="" class="header-img">
         </div>
-      </div>
+      </span>
+      <nav class="navbar">
+        <ul>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+          <li><NuxtLink to="/contact">Contact</NuxtLink></li>
+        </ul>
+      </nav>
     </div>
     <div class="content">
       <NuxtPage/>
     </div>
-    <div class="header">
-      <div class="header-container">
-        <img src="./assets/images/footer.jpg" alt="" class="header-img">
-      </div>
-    </div>
   </div>
 </template>
+
+<script setup>
+const goHome = async () => {
+  await navigateTo('/');
+}
+</script>
+
 
 <style lang="scss">
 @import './assets/styles/colors';
@@ -29,12 +32,35 @@
 body, html{
   background-color: $black;
   color: $white;
-  height: 100vh;
+  margin: 0;
+  padding: 0;
+  height: 100%;
 }
 
 .container {
-  display: flex;
+  max-width: 777px;
+  margin: 0 auto;
+  height: 100vh;
+  display: flex; /* use Flexbox layout */
   flex-direction: column;
-  height: 100%;
+}
+
+.content {
+  flex: 1;
+}
+
+.img-container {
+  height: 50px;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.header-img {
+  max-width: 100%;
+  height: auto;
+  object-fit: cover;
+  object-position: center;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,11 @@
 <template>
+    <div>
+        <h3 class="welcome">
+            Welcome to the blog of Christopher James Smith (CryptoChrisJames)! 
+            This is the place where I discuss technology, entertainment, 
+            and everything else in between. 
+        </h3>
+    </div>
     <span v-if="data">
         <div class="content-list" v-for="post in data" :key="post.id">
             <div @click="goToPost(post._path)">
